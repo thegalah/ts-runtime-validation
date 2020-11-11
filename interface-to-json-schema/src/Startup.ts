95ddf0fb328384d4befdd8a09d0af8ce23bff1da
Matthew Duong <maduong@microsoft.com>
2020-11-11 13:16:48 -0800

    .

--- interface-to-json-schema/src/Startup.ts
+++ interface-to-json-schema/src/Startup.ts
@@ -1,7 +1,8 @@
+import { SchemaDefinition } from "./SchemaDefinition";
 import { validate } from "./validate";
 import "source-map-support/register";
 console.log("hello world");
 const data = {
     name: "someName",
 };
-console.log(validate(data, "#/definitions/SimpleSchema"));
+console.log(validate(data, SchemaDefinition.SIMPLESCHEMA));
