cb8695ce8da49fe1ef65681ee0c75c7717113d5b
Matthew <thegalah@gmail.com>
2022-04-10 11:43:16 -0700

    wip

--- src/SchemaGenerator.ts
+++ src/SchemaGenerator.ts
@@ -1,7 +1,6 @@
 import { fdir } from "fdir";
 import { ICommandOptions } from "./index";
-
-// get all files in a directory synchronously
+import picomatch from "picomatch";
 
 export class SchemaGenerator {
     public constructor(private options: ICommandOptions) {
@@ -11,7 +10,15 @@ export class SchemaGenerator {
     private getMatchingFiles = async () => {
         console.log(this.options);
         const { glob, rootPath } = this.options;
-        const api = new fdir().glob(glob).crawl(rootPath);
+        const api = new fdir().crawlWithOptions(rootPath, {
+            includeBasePath: true,
+            includeDirs: false,
+            filters: [
+                (path) => {
+                    return picomatch.isMatch(path, glob, { contains: true });
+                },
+            ],
+        });
         const files = await api.withPromise();
         console.log(files);
     };
