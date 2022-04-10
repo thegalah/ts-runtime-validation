9e4a6192a840bfc25b30d0b6f64d7d74660b4638
Matthew <thegalah@gmail.com>
2022-04-10 04:39:55 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 #!/usr/bin/env node
 import { program } from "commander";
 
+interface IOptions {
+    readonly glob: string;
+    readonly output: string;
+}
+
 // import { fdir } from "fdir";
 
 program.option("--glob", "Glob file path of typescript files to generate ts-interface -> json-schema validations for.", "*");
+program.option("--output", "Validation schema + typescript interface output directory", "./.ts-runtime-check");
 
 // program.addArgument("<glob>", "Glob file path of typescript files to generate ts-interface -> json-schema validations for.", "*");
 // program.addArgument("<outputPath>", "Validation schema output and validation path"), ".ts-runtime-check";
-program.action((glob, outputPath) => {
-    console.log(`provided glob: ${glob}`);
-    // console.log(`output path: ${outputPath}`);
-});
+// program.action((glob, outputPath) => {
+//     console.log(`provided glob: ${glob}`);
+//     console.log(`output path: ${outputPath}`);
+// });
 
 program.parse();
+
+const options = program.opts<IOptions>();
+console.log(options);
 
 // program.parse();
 
