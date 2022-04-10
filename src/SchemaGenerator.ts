ea537c438a711e5c38789e2cc3586c3a5696763c
Matthew <thegalah@gmail.com>
2022-04-10 14:56:48 -0700

    Update SchemaGenerator.ts

--- src/SchemaGenerator.ts
+++ src/SchemaGenerator.ts
@@ -1,14 +1,15 @@
 import { fdir } from "fdir";
 import { ICommandOptions } from "./index";
+import { resolve } from "path";
+import * as TJS from "typescript-json-schema";
 import picomatch from "picomatch";
 
 export class SchemaGenerator {
     public constructor(private options: ICommandOptions) {
-        this.getMatchingFiles();
+        this.generateJsonSchema();
     }
 
     private getMatchingFiles = async () => {
-        console.log(this.options);
         const { glob, rootPath } = this.options;
         const api = new fdir().crawlWithOptions(rootPath, {
             includeBasePath: true,
@@ -19,6 +20,29 @@ export class SchemaGenerator {
                 },
             ],
         });
-        return await api.withPromise();
+        return (await api.withPromise()) as Array<string>;
+    };
+
+    private generateJsonSchema = async () => {
+        const filesList = await this.getMatchingFiles();
+        const files = filesList.map((fileName) => {
+            return resolve(fileName);
+        });
+        const settings: TJS.PartialArgs = {
+            required: true,
+        };
+
+        const compilerOptions: TJS.CompilerOptions = {
+            strictNullChecks: true,
+        };
+
+        const program = TJS.getProgramFromFiles(files, compilerOptions);
+
+        const generator = TJS.buildGenerator(program, settings);
+        const userDefinedSymbols = generator.getMainFileSymbols(program);
+        userDefinedSymbols.forEach((symbol) => {
+            const schema = generator.getSchemaForSymbol(symbol);
+            console.log(schema);
+        });
     };
 }
