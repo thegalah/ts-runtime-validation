2f2b91cc4b6932d7e6e831a017aac465b1e156b1
Matthew <thegalah@gmail.com>
2022-04-09 23:23:52 -0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import { program } from "commander";
 
 // import { fdir } from "fdir";
 
-program.argument("glob", "Glob file path of typescript files to generate ts-interface -> json-schema validations for.", "*");
-program.argument("outputPath", "Validation schema output and validation path");
+program.argument("<glob>", "Glob file path of typescript files to generate ts-interface -> json-schema validations for.", "*");
+program.argument("<outputPath>", "Validation schema output and validation path");
 program.action((glob, outputPath) => {
     console.log(`provided glob: ${glob}`);
     console.log(`output path: ${outputPath}`);
 });
 
-program.parse();
+// program.parse();
 
 // const args = program.arguments;
 // console.log(args);
