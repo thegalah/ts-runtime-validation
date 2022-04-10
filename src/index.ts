1a0eda4fc1fdf64e3dec5995e535aa9c166ad1fe
Matthew <thegalah@gmail.com>
2022-04-09 22:32:58 -0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-console.log("hello world");
+import { program } from "commander";
+
+program.option("--first").option("-s, --separator <char>");
+program.parse();
+const options = program.opts();
+const limit = options.first ? 1 : undefined;
+console.log(program.args[0].split(options.separator, limit));
