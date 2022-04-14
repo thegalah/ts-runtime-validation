df0c53c6ac6cee746c78132cf253790840a20d74
Matthew <thegalah@gmail.com>
2022-04-14 10:49:46 -0700

    Create code generation library (#3)
    * Restructure
    
    * wip
    
    * bump versions
    
    * Update package.json
    
    * wip
    
    * wip
    
    * Update package.json
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Update package.json
    
    * Executable
    
    * wip
    
    * wip
    
    * wip
    
    * 0.1.8
    
    * wip
    
    * 0.1.9
    
    * wip
    
    * wip
    
    * 0.1.10
    
    * wip
    
    * 0.1.11
    
    * wip
    
    * 0.1.12
    
    * wip
    
    * 0.1.13
    
    * wip
    
    * 0.1.14
    
    * wip
    
    * 0.1.15
    
    * wip
    
    * 0.1.16
    
    * wip
    
    * 0.1.17
    
    * wip
    
    * 0.1.18
    
    * wip
    
    * 0.1.19
    
    * wip
    
    * 0.1.20
    
    * wip
    
    * 0.1.21
    
    * wip
    
    * 0.1.22
    
    * wip
    
    * 0.1.23
    
    * wip
    
    * 0.1.24
    
    * wip
    
    * wip
    
    * 0.1.25
    
    * Update index.ts
    
    * Update index.ts
    
    * 0.1.26
    
    * Update index.ts
    
    * 0.1.27
    
    * Update index.ts
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Update index.ts
    
    * wip
    
    * Update SchemaGenerator.ts
    
    * 0.1.28
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * wip
    
    * 0.1.29
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * Update index.ts
    
    * Update SchemaGenerator.ts
    
    * bump deps
    
    * Update SchemaGenerator.ts
    
    * 0.1.30
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * 0.1.31
    
    * Update SchemaGenerator.ts
    
    * Update index.ts
    
    * Peer dependency
    
    * Update tsconfig.json
    
    * 0.1.32
    
    * .
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * Update SchemaGenerator.ts
    
    * wip
    
    * Update SchemaGenerator.ts
    
    * wip
    
    * wip
    
    * Update README.md
    
    * Update README.md
    
    * Update package.json
    
    * 1.0.1
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update package.json
    
    * Update README.md

---
+++ src/SchemaGenerator.ts
@@ -0,0 +1,242 @@
+import { fdir } from "fdir";
+import { ICommandOptions } from "./index";
+import { resolve } from "path";
+import * as TJS from "typescript-json-schema";
+import fs from "fs";
+import picomatch from "picomatch";
+import path from "path";
+import {
+    Project,
+    IndentationText,
+    NewLineKind,
+    QuoteKind,
+    StructureKind,
+    VariableDeclarationKind,
+    CodeBlockWriter,
+    ProjectOptions,
+    SourceFileCreateOptions,
+} from "ts-morph";
+
+const defaultProjectSettings: ProjectOptions = {
+    manipulationSettings: {
+        indentationText: IndentationText.FourSpaces,
+        newLineKind: NewLineKind.LineFeed,
+        quoteKind: QuoteKind.Double,
+        usePrefixAndSuffixTextForRename: false,
+        useTrailingCommas: true,
+    },
+};
+
+const defaultCreateFileOptions: SourceFileCreateOptions = {
+    overwrite: true,
+};
+
+const validationSchemaFileName = "validation.schema.json";
+const schemaDefinitionFileName = "SchemaDefinition.ts";
+
+export class SchemaGenerator {
+    private outputPath = path.join(this.options.rootPath, this.options.output);
+    private jsonSchemaOutputFile = path.join(this.options.rootPath, this.options.output, validationSchemaFileName);
+    private tsSchemaDefinitionOutputFile = path.join(this.options.rootPath, this.options.output, schemaDefinitionFileName);
+    private isValidSchemaOutputFile = path.join(this.options.rootPath, this.options.output, "isSchemaValid.ts");
+
+    public constructor(private options: ICommandOptions) {
+        this.generateOutput();
+    }
+
+    private generateOutput = async () => {
+        const { helpers } = this.options;
+        const fileList = await this.getMatchingFiles();
+        console.log(`Found ${fileList.length} schema file(s)`);
+        const map = await this.getJsonSchemaMap(fileList);
+        console.log(`Generating ${map.size} validation schema(s)`);
+        this.writeSchemaMapToValidationSchema(map);
+        if (helpers === false) {
+            console.log("Skipping helper file generation");
+            return;
+        }
+        await this.writeSchemaMapToValidationTypes(map, fileList);
+        this.writeValidatorFunction();
+    };
+
+    private getMatchingFiles = async () => {
+        const { glob, rootPath } = this.options;
+        const api = new fdir().crawlWithOptions(rootPath, {
+            includeBasePath: true,
+            includeDirs: false,
+            filters: [
+                (path) => {
+                    return picomatch.isMatch(path, glob, { contains: true });
+                },
+            ],
+        });
+        return api.withPromise() as Promise<Array<string>>;
+    };
+
+    private getJsonSchemaMap = async (filesList: Array<string>) => {
+        const schemaMap = new Map<string, TJS.Definition>();
+        const files = filesList.map((fileName) => {
+            return resolve(fileName);
+        });
+        const settings: TJS.PartialArgs = {
+            required: true,
+            titles: true,
+            aliasRef: true,
+            ref: true,
+            noExtraProps: true,
+            propOrder: true,
+        };
+
+        const compilerOptions: TJS.CompilerOptions = {
+            strictNullChecks: true,
+        };
+
+        const program = TJS.getProgramFromFiles(files, compilerOptions);
+
+        const generator = TJS.buildGenerator(program, settings);
+        const userDefinedSymbols = generator?.getMainFileSymbols(program) ?? [];
+        userDefinedSymbols.forEach((symbol) => {
+            if (schemaMap.has(symbol)) {
+                throw new Error(`Duplicate symbol "${symbol}" found.`);
+            }
+            const schema = generator?.getSchemaForSymbol(symbol);
+            if (schema) {
+                schemaMap.set(symbol, schema);
+            }
+        });
+
+        return schemaMap;
+    };
+
+    private getSchemaVersion = (schemaMap: Map<string, TJS.Definition>) => {
+        const firstEntry = schemaMap.values().next().value;
+        return firstEntry["$schema"] ?? "";
+    };
+
+    private ensureOutputPathExists = () => {
+        if (!fs.existsSync(this.outputPath)) {
+            fs.mkdirSync(this.outputPath, { recursive: true });
+        }
+    };
+
+    private writeSchemaMapToValidationSchema = (schemaMap: Map<string, TJS.Definition>) => {
+        const definitions: { [id: string]: TJS.Definition } = {};
+        schemaMap.forEach((schema, key) => {
+            definitions[key] = schema;
+        });
+        const outputBuffer: TJS.Definition = {
+            $schema: this.getSchemaVersion(schemaMap),
+            definitions,
+        };
+
+        this.ensureOutputPathExists();
+        fs.writeFileSync(this.jsonSchemaOutputFile, JSON.stringify(outputBuffer, null, 4));
+    };
+
+    private writeSchemaMapToValidationTypes = async (schemaMap: Map<string, TJS.Definition>, fileList: Array<string>) => {
+        const project = new Project(defaultProjectSettings);
+
+        const symbols = Array.from(schemaMap.keys()).filter((symbol) => {
+            return symbol !== "ISchema" && symbol !== "Schemas";
+        });
+
+        const readerProject = new Project();
+        readerProject.addSourceFilesAtPaths(fileList);
+
+        const importMap = new Map<string, Array<string>>();
+        fileList.forEach((file) => {
+            const dir = path.dirname(file);
+            const fileWithoutExtension = path.parse(file).name;
+            const relativeFilePath = path.relative(this.outputPath, dir);
+            const importPath = `${relativeFilePath}/${fileWithoutExtension}`;
+            const source = readerProject.getSourceFile(file);
+            source?.getInterfaces().forEach((interfaceDeclaration) => {
+                const structure = interfaceDeclaration.getStructure();
+                const namedImports = importMap.get(importPath) ?? [];
+                namedImports.push(structure.name);
+                importMap.set(importPath, namedImports);
+            });
+        });
+
+        const sourceFile = project.createSourceFile(this.tsSchemaDefinitionOutputFile, {}, defaultCreateFileOptions);
+        importMap.forEach((namedImports, importPath) => {
+            sourceFile.addImportDeclaration({ namedImports, moduleSpecifier: importPath });
+        });
+
+        sourceFile.addVariableStatement({
+            declarationKind: VariableDeclarationKind.Const,
+            declarations: [
+                {
+                    name: "schemas",
+                    type: "Record<keyof ISchema, string>",
+                    initializer: (writer: CodeBlockWriter) => {
+                        writer.writeLine(`{`);
+                        symbols.forEach((symbol) => {
+                            writer.writeLine(`["#/definitions/${symbol}"] : "${symbol}",`);
+                        }),
+                            writer.writeLine(`}`);
+                    },
+                },
+            ],
+        });
+
+        sourceFile.addInterface({
+            kind: StructureKind.Interface,
+            name: "ISchema",
+            isExported: false,
+            properties: symbols.map((symbol) => {
+                return { name: `readonly ["#/definitions/${symbol}"]`, type: symbol };
+            }),
+        });
+
+        sourceFile.addExportDeclaration({
+            namedExports: ["schemas", "ISchema"],
+        });
+
+        await project.save();
+    };
+
+    private writeValidatorFunction = async () => {
+        const project = new Project(defaultProjectSettings);
+        const sourceFile = project.createSourceFile(this.isValidSchemaOutputFile, {}, defaultCreateFileOptions);
+        sourceFile.addImportDeclaration({ namespaceImport: "schema", moduleSpecifier: `./${validationSchemaFileName}` });
+        sourceFile.addImportDeclaration({ defaultImport: "Ajv", moduleSpecifier: "ajv" });
+        sourceFile.addImportDeclaration({
+            namedImports: ["ISchema", "schemas"],
+            moduleSpecifier: `./${path.parse(schemaDefinitionFileName).name}`,
+        });
+
+        sourceFile.addVariableStatement({
+            declarationKind: VariableDeclarationKind.Const,
+            declarations: [
+                {
+                    name: "validator",
+                    initializer: (writer: CodeBlockWriter) => {
+                        writer.writeLine(`new Ajv({ allErrors: true });`);
+                        writer.writeLine(`validator.compile(schema)`);
+                    },
+                },
+            ],
+        });
+
+        sourceFile.addVariableStatement({
+            declarationKind: VariableDeclarationKind.Const,
+            declarations: [
+                {
+                    name: "isValidSchema",
+                    initializer: (writer: CodeBlockWriter) => {
+                        writer.writeLine(`<T extends keyof typeof schemas>(data: unknown, schemaKeyRef: T): data is ISchema[T] => {`);
+                        writer.writeLine(`validator.validate(schemaKeyRef as string, data);`);
+                        writer.writeLine(`return Boolean(validator.errors) === false;`);
+                        writer.writeLine(`}`);
+                    },
+                },
+            ],
+        });
+
+        sourceFile.addExportDeclaration({
+            namedExports: ["validator", "isValidSchema"],
+        });
+        await project.save();
+    };
+}
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+import { SchemaGenerator } from "./SchemaGenerator";
+import { program } from "commander";
+
+const defaultGlobPattern = "*.jsonschema.{ts,tsx}";
+
+const defaultRootPath = "./src";
+const defaultOutputFolder = "./.ts-runtime-check";
+
+export interface ICommandOptions {
+    readonly glob: string;
+    readonly rootPath: string;
+    readonly output: string;
+    readonly helpers: boolean;
+}
+
+program.option(
+    "--glob",
+    `Glob file path of typescript files to generate ts-interface -> json-schema validations - default: ${defaultGlobPattern}`,
+    defaultGlobPattern
+);
+program.option("--rootPath", `RootPath of source - default: ${defaultRootPath}`, defaultRootPath);
+program.option(
+    "--output",
+    `Validation schema + typescript interface output directory (relative to root path) - default: ${defaultOutputFolder}`,
+    defaultOutputFolder
+);
+program.option("--no-helpers", "Only generate JSON schema without typescript helper files", true);
+
+program.parse();
+
+const options = program.opts<ICommandOptions>();
+
+new SchemaGenerator(options);
