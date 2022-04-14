f08dfd54eb1089fc11b60d2c8047b9e310a5d1e5
Matthew <thegalah@gmail.com>
2022-04-14 09:10:54 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@
 import { SchemaGenerator } from "./SchemaGenerator";
 import { program } from "commander";
 
+const defaultRootPath = "./src";
+const defaultOutputFolder = "./ts-runtime-check";
+
 export interface ICommandOptions {
     readonly glob: string;
     readonly rootPath: string;
@@ -14,8 +17,12 @@ program.option(
     "Glob file path of typescript files to generate ts-interface -> json-schema validations for.",
     "*.jsonschema.{ts,tsx}"
 );
-program.option("--rootPath", "RootPath to search", "./src");
-program.option("--output", "Validation schema + typescript interface output directory (relative to root path)", "./.ts-runtime-check");
+program.option("--rootPath", `RootPath of source - default: ${defaultRootPath}`, defaultRootPath);
+program.option(
+    "--output",
+    `Validation schema + typescript interface output directory (relative to root path) - default: ${defaultOutputFolder}`,
+    defaultOutputFolder
+);
 
 program.parse();
 
