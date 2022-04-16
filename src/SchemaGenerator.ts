8e2531c371ff6f13a98a41e781f25158ce45768c
Matthew <thegalah@gmail.com>
2022-04-16 16:04:37 -0700

    Update SchemaGenerator.ts

--- src/SchemaGenerator.ts
+++ src/SchemaGenerator.ts
@@ -89,8 +89,6 @@ export class SchemaGenerator {
                 type: "*",
                 additionalProperties,
                 encodeRefs: false,
-                topRef: false,
-                expose: "none",
                 sortProps: true,
             };
 
