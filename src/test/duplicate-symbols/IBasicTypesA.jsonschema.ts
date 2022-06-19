ae3207b1bac4ccb3c9e7915122eab1586eb67fbe
Matthew <thegalah@gmail.com>
2022-06-19 01:56:22 -0700

    wip

---
+++ src/test/duplicate-symbols/IBasicTypesA.jsonschema.ts
@@ -0,0 +1,6 @@
+import { IBaseType } from "./IBaseType";
+
+export interface IBasicTypesA {
+    readonly propertyA: IBaseType;
+    readonly propertyB: IBaseType;
+}
--- src/test/duplicate-symbols/IBasicTypesB.jsonschema.ts
+++ src/test/duplicate-symbols/IBasicTypesB.jsonschema.ts
@@ -1,9 +1,5 @@
 import { IBaseType } from "./IBaseType";
 
-export interface IBasicTypes {
-    readonly propertyA: IBaseType;
-    readonly propertyB: IBaseType;
-}
 export interface IBasicTypesB {
     readonly propertyA: IBaseType;
     readonly propertyB: IBaseType;
