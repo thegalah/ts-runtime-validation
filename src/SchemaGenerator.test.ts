924eb357aa5382f72590d8865d58c0afea5e474c
Matthew <thegalah@gmail.com>
2022-06-18 23:08:51 -0700

    Update SchemaGenerator.test.ts

--- src/SchemaGenerator.test.ts
+++ src/SchemaGenerator.test.ts
@@ -1,15 +1,18 @@
+import path from "path";
 import { ICommandOptions } from "./index";
 import { SchemaGenerator } from "./SchemaGenerator";
+
 describe("SchemaGenerator", () => {
     test("it should do stuff", async () => {
         const options: ICommandOptions = {
             glob: "*.jsonschema.ts",
-            rootPath: "./test/basic-scenario",
+            rootPath: path.resolve(__dirname, "./test"),
             output: "./validation-types",
             helpers: false,
             additionalProperties: false,
             tsconfigPath: "",
         };
+        // expect(__dirname).toStrictEqual(true);
         const generator = new SchemaGenerator(options);
         await generator.Generate();
         expect(true).toStrictEqual(true);
