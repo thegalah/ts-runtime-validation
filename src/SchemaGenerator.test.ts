10e45fa05412ef5e54bcdbb00d92ef1b66887c5c
Matthew <thegalah@gmail.com>
2022-06-18 23:20:31 -0700

    Update SchemaGenerator.test.ts

--- src/SchemaGenerator.test.ts
+++ src/SchemaGenerator.test.ts
@@ -1,6 +1,15 @@
+import fs from "fs";
 import path from "path";
 import { ICommandOptions } from "./index";
 import { SchemaGenerator } from "./SchemaGenerator";
+
+const cleanupTestOutput = () => {
+    const outputDir = path.resolve(__dirname, "./test/output");
+    fs.rmdirSync(outputDir, { recursive: true });
+};
+
+beforeAll(cleanupTestOutput);
+afterAll(cleanupTestOutput);
 
 describe("SchemaGenerator", () => {
     test("it should do stuff", async () => {
